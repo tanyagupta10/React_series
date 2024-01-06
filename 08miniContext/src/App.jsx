b8c261901c7d2b646fbafcbbb3793f7aeed163fd
tanyagupta10 <tg425312@gmail.com>
2024-01-06 17:20:40 +0530

    save changes

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+// import './App.css'
+
+import Login from "./component/Login"
+import Profile from "./component/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+       <Login/>
+       <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
